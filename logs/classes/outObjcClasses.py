9085c33bf8fe272c5f437565f54b08a7aba029fc
pome-ta <pome-ta@users.noreply.github.com>
2021-10-07 13:56:14 +0900

    up

--- logs/classes/outObjcClasses.py
+++ logs/classes/outObjcClasses.py
@@ -3,4 +3,9 @@
 # https://forum.omz-software.com/topic/2808/bug-objc_util-and-dir
 
 from objc_util import ObjCClass
-print('\n'.join(ObjCClass.get_names('')))
+
+all = ObjCClass.get_names('')
+[print(cls) for cls in all if '_' != cls[0]]
+
+# --- _all
+#print('\n'.join(ObjCClass.get_names('')))
