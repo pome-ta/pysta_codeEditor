c331b42651a6f2c421c631deec98d25ff5aa7c9e
pome-ta <pome-ta@users.noreply.github.com>
2021-10-07 14:22:51 +0900

    pyobjLog.txt

--- logs/classes/outObjcClasses.py
+++ logs/classes/outObjcClasses.py
@@ -5,7 +5,32 @@
 from objc_util import ObjCClass
 
 all = ObjCClass.get_names('')
-[print(cls) for cls in all if '_' != cls[0]]
 
-# --- _all
-#print('\n'.join(ObjCClass.get_names('')))
+
+def out_log(log):
+  print('\n'.join(log))
+
+
+def main_log():
+  out = [cls for cls in all if '_' != cls[0]]
+  out_log(out)
+
+
+def _all_log():
+  out = [cls for cls in all]
+  out_log(out)
+
+
+def dunder_log():
+  out = [cls for cls in all if '_' == cls[0]]
+  out_log(out)
+
+
+def pyobj_log():
+  out = [cls for cls in all if ('OM' == cls[:2]) or ('PA' == cls[:2]) or ('PY' == cls[:2])]
+  out_log(out)
+
+
+if __name__ == '__main__':
+  pyobj_log()
+
