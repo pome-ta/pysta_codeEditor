e3d18357a4e5146933d08032af944e6fdb3f3331
pome-ta <pome-ta@users.noreply.github.com>
2021-10-09 02:20:27 +0900

    up

--- playground/shaderCodeEditor/__main__.py
+++ playground/shaderCodeEditor/__main__.py
@@ -1,112 +1,201 @@
-# xxx: ui.View に統合
-from pathlib import Path
-import scene
 import ui
+import scene
+from objc_util import ObjCClass, on_main_thread, ObjCInstance
 
-src = '''
-precision highp float;
+import pdbg
+
+
+
+
+src = '''precision highp float;
 
 uniform float u_time;
-uniform vec2 u_sprite_size;
-uniform float u_scale;
-uniform sampler2D u_texture;
-uniform vec4 u_tint_color;
-uniform vec4 u_fill_color;
 varying vec2 v_tex_coord;
 
 
-float saturate(float f){
-   return clamp(f, 0.0, 1.0);
-}
-
-vec2 rotate(vec2 uv, vec2 center, float angle){
-  vec2 c1 = center * vec2(-1.0);
-  vec2 adduv = c1 + uv;
-  
-  float a1 = sin(angle) * -1.0;
-  float a2 = cos(angle);
-  vec2 ap1 = vec2 (a2, a1);
-  vec2 ap2 = vec2(sin(angle), a2);
-  
-  float uu = dot(adduv, ap1);
-  float vv = dot(adduv, ap2);
-  
-  vec2 uuvv = vec2(uu, vv);
-  vec2 result = center + uuvv;
-  return result;
-}
-
 void main(){
+  //float pi = acos(-1.0);
   float t = u_time;
   vec2 uv = v_tex_coord;
-  //uv = (uv - vec2(0.5)) * 2.0;
   
-  vec2 p = abs((uv- vec2(0.5)) / 0.5);
-  float l = 1.0 - length(p);
   
-  float n = saturate(l) * 10.0;
-  
-  float pi = acos(-1.0);
-  float m = (t * 0.5) * (pi * 2.0);
-  
-  float o = n + m;
-  
-  vec2 mix = rotate(uv, vec2(0.5), o);
-  
-  //mix = mix / 2.0 + vec2(0.5);
-  uv = uv / 2.0 + vec2(0.5);
-  //if (uv.x < 0.0 || uv.x > 1.0 || uv.y < 0.0 || uv.y > 1.0)discard;
-  gl_FragColor = vec4(mix, 0.0, 1.0);
-  //gl_FragColor = texture2D(u_texture,uv.xy);
+  vec3 out_color = vec3(uv.x, uv.y, abs(sin(t)));
+  gl_FragColor = vec4(out_color, 1.0);
   
 }
 '''
 
-
 class ShaderScene(scene.Scene):
   def setup(self):
-
     self.shdr = scene.SpriteNode(parent=self)
-    #self.shdr.texture = img
-    _x = self.size.x * .88
-    _y = self.size.y * .64
+    _x = self.size.x
+    #_y = self.size.x
     self.shdr.size = (_x, _x)
-    #shdr.size=self.size
     self.shdr.shader = scene.Shader(src)
+    
     self.shdr.shader.set_uniform('u_resolution', (_x, _x))
     # todo: Initial position before touching
     self.shdr.shader.set_uniform('u_offset', (0.5, 0.5))
-
+    
   def did_change_size(self):
-    # todo: Center the image
-    self.shdr.position = self.size / 2
-    _x = self.size.x * .88
-    _y = self.size.y * .64
+    _x = self.size.x
     self.shdr.size = (_x, _x)
+    sx = _x / 2
+    self.shdr.position = (sx, self.size.y - sx)
+    
+    
 
+class CodeEditorView:
+  @on_main_thread
+  def __init__(self, mode='javascript', ext_kb=True, *args, **kwargs):
+    valid_modes = {
+      'python': 'OMPythonSyntaxHighlighter',
+      'html': 'OMHTMLSyntaxHighlighter',
+      'javascript': 'OMJavaScriptSyntaxHighlighter',
+      'markdown': 'OMMarkdownSyntaxHighlighter',
+      'text': 'OMBaseSyntaxHighlighter'
+    }
+    if mode not in valid_modes:
+      raise ValueError('invalid syntax mode')
+    
+    OMTextEditorView = ObjCClass('OMTextEditorView')
+    
+    OMSyntaxHighlighterTheme = ObjCClass('OMSyntaxHighlighterTheme')
+    SyntaxHighlighter = ObjCClass(valid_modes[mode])
+    
+    PA2UITheme = ObjCClass('PA2UITheme')
+    theme_dict = PA2UITheme.sharedTheme().themeDict().mutableCopy()
+    #theme_dict.autorelease()
+    theme_dict['font-family'] = 'Source Code Pro'
+    theme_dict['font-size'] = 14
+    theme = OMSyntaxHighlighterTheme.alloc().initWithDictionary_(theme_dict)
+    
+    #f = CGRect(CGPoint(0, 0), CGSize(100.0, 100.0))
+    f = ((0.0, 0.0), (100.0, 100.0))
+    self.editor_view = OMTextEditorView.alloc().initWithFrame_syntaxHighlighterClass_theme_(f, SyntaxHighlighter, theme)
+    self.editor_view.textView().setAutocapitalizationType_(0)
+    self.editor_view.textView().setAutocorrectionType_(1)
+    flex_width, flex_height = (1 << 1), (1 << 4)
+    self.editor_view.setAutoresizingMask_(flex_width | flex_height)
+    #margins = UIEdgeInsets(16, 10, 16, 10)
+    #self.editor_view.setMarginsForPortrait_landscape_(margins, margins)
+    if ext_kb:
+      kb_types = {
+        'python': 'KeyboardAccessoryTypePythonCompact',
+        'markdown': 'KeyboardAccessoryTypeMarkdownWithoutSnippets',
+        'html': 'KeyboardAccessoryTypeHTML',
+        'javascript': 'KeyboardAccessoryTypeHTML'
+      }
+      kb_type = kb_types.get(mode)
+      if kb_type:
+        OMKeyboardAccessoryView = ObjCClass('OMKeyboardAccessoryView')
+        accessory_view = OMKeyboardAccessoryView.alloc().initWithType_dark_(kb_type, False)#.autorelease()
+        self.editor_view.setKeyboardAccessoryView_(accessory_view)
+    
+    self.editor_view.setOpaque_(0)
+    self.editor_view.setBackgroundColor_((0.0, 0.0, 0.0, 0.0))
+  
+  
+  @property
+  #@on_main_thread
+  def text(self):
+    text_view = self.editor_view.textView()
+    text = text_view.text()
+    return str(text)
 
+  @text.setter
+  #@on_main_thread
+  def text(self, new_text):
+    if not isinstance(new_text, str):
+      raise TypeError('expected string/unicode')
+    text_view = self.editor_view.textView()
+    text_view.setText_(new_text)
+    
+
+  #@on_main_thread
+  def insert_text(self, text):
+    if not isinstance(text, basestring):
+      raise TypeError('expected string/unicode')
+    text_view = self.editor_view.textView()
+    text_view.insertText_(text)
+
+  #@on_main_thread
+  def replace_range(self, range, text):
+    text_view = self.editor_view.textView()
+    ns_range = NSRange(range[0], range[1] - range[0])
+    text_range = ObjCClass('OMTextRange').rangeWithNSRange_(ns_range)
+    text_view.replaceRange_withText_(text_range, text)
+
+  @property
+  #@on_main_thread
+  def selected_range(self):
+    text_view = self.editor_view.textView()
+    range = text_view.selectedRange()
+    return (range.location, range.location + range.length)
+
+  @selected_range.setter
+  #@on_main_thread
+  def selected_range(self, new_value):
+    text_view = self.editor_view.textView()
+    range = NSRange(new_value[0], new_value[1] - new_value[0])
+    text_view.setSelectedRange_(range)
+
+  #@on_main_thread
+  def begin_editing(self):
+    text_view = self.editor_view.textView()
+    text_view.becomeFirstResponder()
+    print('begin_editing')
+
+  #@on_main_thread
+  def end_editing(self):
+    text_view = self.editor_view.textView()
+    text_view.resignFirstResponder()
+    print('end_editing')
+
+  
+  
+  
 class MainView(ui.View):
   def __init__(self, *args, **kwargs):
     ui.View.__init__(self, *args, **kwargs)
-    #interval = 60
-    #self.update_interval = 1 / interval
-    self.bg_color = 'maroon'
-
+    #self.bg_color = 'maroon'
     self.shader_scene = ShaderScene()
     self.ui_scene = scene.SceneView()
     self.ui_scene.shows_fps = True
     self.ui_scene.frame_interval = 2  # 30fps
     self.ui_scene.scene = self.shader_scene
-
+    
     self.add_subview(self.ui_scene)
+    
+    self.cev = CodeEditorView()
+    self.cev.text = src
+    
+    self.objc_instance.addSubview_(self.cev.editor_view)
+    self.reload_btn()
 
   def layout(self):
     self.ui_scene.width = self.width
     self.ui_scene.height = self.height
     self.shader_scene.did_change_size()
 
+  def reload_btn(self):
+    self.close_btn = self.create_btn('iob:ios7_refresh_outline_32')
+    self.close_btn.action = (lambda sender: self.reload_src())
+    self.right_button_items = [self.close_btn]
+    #self.add_subview(self.close_btn)
+
+  def create_btn(self, icon):
+    btn_icon = ui.Image.named(icon)
+    return ui.ButtonItem(image=btn_icon)
+    
+  def reload_src(self):
+    #print('hofe')
+    #print(self.cev.text)
+    self.shader_scene.shdr.shader = scene.Shader(self.cev.text)
+    
 
 if __name__ == '__main__':
   view = MainView()
   view.present(style='fullscreen', orientations=['portrait'])
+  #view.present()
 
